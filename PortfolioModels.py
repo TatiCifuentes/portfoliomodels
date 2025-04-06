83cb9343d1abde1f51d2f36190e7484b84a7b0b9
TatiCifuentes <tatacifuentes12345@gmail.com>
2025-04-06 17:16:28 -0500

    Update PortfolioModels.py

--- PortfolioModels.py
+++ PortfolioModels.py
@@ -281,7 +281,7 @@ def medidas(retornos,rindice,wpo):
   varp = np.percentile(rph,5)
   cvarp = rph[rph < varp].mean().item()
   ra=(rph.mean()-rindice.mean()).round(6) 
-  tracking_errror=(rph.values-rindice.iloc[:,0].values).std()
+  tracking_error=(rph.values-rindice.iloc[:,0].values).std()
 
   medidas = {'Retorno': rp, 'Volatilidad':sigmap, 'Sharpe': sharpep, 'Beta':betap,'Treynor':treynorp,'Sortino': sortinop,
              'Omega': omegap.item(), 'CVaR 95%': cvarp, 'Tracking Error': tracking_error }
